5d94639bba99201f0960e481b3e8ae1098e953c1
tsokac2 <tsokac2@gmail.com>
2024-04-02 09:51:04 +0100

    Updating

--- Script_02_APIs/get_api.py
+++ Script_02_APIs/get_api.py
@@ -1,9 +1,7 @@
 import requests
 
-url = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=36e6f30176404586a22893235b25572c"
-
-getRequest = requests.get(url)
-
-content = getRequest.json()
-
-print(content)
+url = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=<your_api_key>"
+get_Request = requests.get(url)
+content = get_Request.json()
+print(type(content))
+print(content['articles'][0]['url'])
