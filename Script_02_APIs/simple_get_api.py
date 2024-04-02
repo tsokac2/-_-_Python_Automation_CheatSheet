49e9678b210ec99b8c05805f4e1c64514b408f4c
tsokac2 <tsokac2@gmail.com>
2024-04-02 10:27:30 +0100

    Updating

--- Script_02_APIs/simple_get_api.py
+++ Script_02_APIs/simple_get_api.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=<your_api_key>"
+url = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=36e6f30176404586a22893235b25572c"
 get_Request = requests.get(url)
 content = get_Request.json()
 print(type(content))
