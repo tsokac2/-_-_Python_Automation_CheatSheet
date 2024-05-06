183871226265c5bb42d784f8f7763158576a1326
Tomislav <tsokac2@gmail.com>
2024-05-06 12:34:00 +0100

    Updating

---
+++ Script_04_Auto_Emails/emails_modify_attachment.py
@@ -0,0 +1,29 @@
+import yagmail
+import os
+import pandas
+
+SENDER = 'email_sender@mail.com'
+
+yag = yagmail.SMTP(user=SENDER, password=os.getenv('PASSWORD')) # PASSWORD value stored in env file
+df = pandas.read_csv('contacts_attachments.csv')
+
+def generate_file(filename, content):
+    with open(filename, 'w') as file:
+        file.write(content)
+
+for index, row in df.iterrows():
+    name = row['name']
+    amount = row['amount']
+    receiver_email = row['email']
+
+    generate_file(row['name'], row['amount'])
+
+    subject = "This is the subject!"
+    contents = [f""" 
+    Hey, {name} you have to pay {amount}
+    Bill is attached!""",
+    name,
+    ]
+
+    yag.send(to=receiver_email, subject=subject, contents=contents)
+    print["Email Sent"]
