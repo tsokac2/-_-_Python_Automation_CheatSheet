72bbe0bbc43c0a8cbab57a7f81d1936df4841cdc
Tomislav <tsokac2@gmail.com>
2024-05-15 11:14:56 +0100

    Updating

--- Script_04_Auto_Emails/emails_script_HTML_otulook_hotmail.py
+++ Script_04_Auto_Emails/emails_script_HTML_otulook_hotmail.py
@@ -1,18 +1,30 @@
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 SENDER = 'youremail@hotmail.com'
 RECEIVER = 'receiver@gmail.com'
 PASSWORD = '<import_from_env_enviroment>' # os.getenv('PASSWORD')
 
-message = """\
-Subject: Hello Hello
+message = MIMEMultipart()
+message['From'] = SENDER
+message['To'] = RECEIVER
+message['Subject'] = 'Hello again!'
 
-This is Tom!
-Just wanted to say hi!
+body = """
+<h2>Hi There </h2>
+Today is a very nice day!
+Let's hope for more!
 """
+
+mimetext = MIMEText(body, 'html')
+message.attach(mimetext)
+
 
 SERVER = smtplib.SMTP('smtp.office365.com', 587)
 SERVER.startls()
 SERVER.login(SENDER, PASSWORD)
-SERVER.sendmail(SENDER, RECEIVER, message)
+message_text = message.as_string()
+print(message_text)
+SERVER.sendmail(SENDER, RECEIVER, message_text)
 SERVER.quit()
